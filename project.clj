5d763e31b5829170b0f81757abe459d604e498f0
Peter Graham <peterghm@gmail.com>
2011-09-18 09:52:05 -0400

    just needed to scale heroku web worker
    This reverts commit 7d1c5d83f65b9e7440e98f01d97f957437503222.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject hello-world "0.0.1"
+(defproject demo "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
 	         [ring/ring-jetty-adapter "0.3.9"]])
