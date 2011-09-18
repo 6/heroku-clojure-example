af5475a975ed39b976d7ccaa8596a924d8a197ff
Peter Graham <peterghm@gmail.com>
2011-09-18 10:30:45 -0400

    upgrade ring-jetty-adapter, install compojure

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject demo "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
-	         [ring/ring-jetty-adapter "0.3.9"]])
+                 [ring/ring-jetty-adapter "0.3.10"]
+                 [compojure "0.6.4"]])
