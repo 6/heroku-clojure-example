7d1c5d83f65b9e7440e98f01d97f957437503222
Peter Graham <peterghm@gmail.com>
2011-09-18 09:47:42 -0400

    try again

--- project.clj
+++ project.clj
@@ -1,4 +1,3 @@
-(defproject demo "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject hello-world "0.0.1"
   :dependencies [[org.clojure/clojure "1.2.1"]
 	         [ring/ring-jetty-adapter "0.3.9"]])
